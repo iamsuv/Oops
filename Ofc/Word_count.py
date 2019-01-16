437ce076231a1406c4244942526c46c31048546a
Suv <subhashisbiswal123@gmail.com>
2019-01-16 18:11:48 +0530

    16/01/19
    Signed-off-by: Suv <subhashisbiswal123@gmail.com>

---
+++ Ofc/Word_count.py
@@ -0,0 +1,11 @@
+
+
+# class Word:
+#     def __init__(self):pass
+#
+#     def count():
+#         for
+
+str="This is an awesome ocassion. This has never happened before. "
+for i in str where :
+    print(i)
